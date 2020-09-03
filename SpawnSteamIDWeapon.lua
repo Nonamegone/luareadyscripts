6421ec52bf63a72d8f933df51d0ea4a3364a234d
Nonamegone <65653743+Nonamegone@users.noreply.github.com>
2020-09-03 16:27:12 +0400

    Update SpawnSteamIDWeapon.lua

--- SpawnSteamIDWeapon.lua
+++ SpawnSteamIDWeapon.lua
@@ -1,4 +1,4 @@
-hook.Add( "PlayerSpawn", "SteamIDSpawnWeapon", function( ply, strText, bTeam, bDead ) -- Хук спавна
+hook.Add( "PlayerSpawn", "SteamIDSpawnWeapon", function( ply ) -- Хук спавна
 	if ply:SteamID() == "Ваш стим айди" then -- Если стим айди = ?? то
 		ply:Give("weapon_fists", false ) -- Выдача оружия, (true или false это есть ли патроны в нем)
 	end -- Конец 2 строки
